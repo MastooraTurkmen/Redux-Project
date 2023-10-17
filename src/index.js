277e42ad0f45c814401a0c3e21d646fb6df96b9e
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-17 09:31:44 +0430

    redux store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { store } from './store'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
