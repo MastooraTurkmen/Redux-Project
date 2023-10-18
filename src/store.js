fcbcf02072a49fc7cef71af07ec8b29f1e255b83
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-18 09:48:02 +0430

    redux cart

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import './features/cart/cartSlice';
+import cartReducer from './features/cart/cartSlice';
 
 export const store = configureStore({
     reducer: {
