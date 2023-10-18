99cf7cdd238836c08bc360fcb711acfd253a4da6
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-18 10:12:48 +0430

    redux navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <div className="nav-container">
                     <CartIcon />
                     <div className="amount-container">
-                        
+                        <p className="total-amount"></p>
                     </div>
                 </div>
             </div>
