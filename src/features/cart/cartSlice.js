5eea26f93ed6541c49e85a234efa89f38a728ba4
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-19 10:34:10 +0430

    redux cartItem

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
 
 const initialState = {
-    cartItems: [],
+    cartItems: cartItems,
     amount: 0,
     total: 0,
     isLoading: true,
