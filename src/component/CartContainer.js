a71ab71d781ed78942e999c13a11aadddce31791
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-19 10:46:30 +0430

    redux cartContainer

--- src/component/CartContainer.js
+++ src/component/CartContainer.js
@@ -2,6 +2,8 @@ import CartItem from "./CartItem"
 import { useSelector } from "react-redux"
 
 const CartContainer = () => {
+    const { cartItems, amount, total } = useSelector((store) => store.cart)
+
     return (
         <div>CartContainer</div>
     )
