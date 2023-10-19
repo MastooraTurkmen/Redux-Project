3fe544a03a4fbc065951a8f3a7e2b6cfef731883
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-19 10:46:53 +0430

    redux cartContainer

--- src/component/CartContainer.js
+++ src/component/CartContainer.js
@@ -4,6 +4,14 @@ import { useSelector } from "react-redux"
 const CartContainer = () => {
     const { cartItems, amount, total } = useSelector((store) => store.cart)
 
+    if (amount < 1) {
+        return (
+            <section className="cart">
+
+            </section>
+        )
+    }
+
     return (
         <div>CartContainer</div>
     )
