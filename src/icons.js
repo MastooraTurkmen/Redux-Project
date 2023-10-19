dee1386aaad5293d0b2f9e0aa9a12cfbd0aff129
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-19 10:31:26 +0430

    redux icon

--- src/icons.js
+++ src/icons.js
@@ -49,12 +49,5 @@ export const ChevronUp = () => {
 
 
 export const Testing = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor" className="w-6 h-6"
-  >
   </svg>
 )
