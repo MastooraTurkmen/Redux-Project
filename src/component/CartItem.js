d6dff2ad7f1e61575873c85f06d0dcd0e5de732b
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-20 11:02:18 +0430

    redux cartItem

--- src/component/CartItem.js
+++ src/component/CartItem.js
@@ -10,7 +10,9 @@ const CartItem = ({ id, img, title, price, amount }) => {
                 <button className="remove-btn">remove</button>
             </div>
             <div>
-                
+                <button className="amount-btn">
+                    
+                </button>
             </div>
         </article>
     )
