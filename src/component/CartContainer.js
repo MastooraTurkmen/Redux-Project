a8a76cf9da08bb63a649ca423890c4393007c8ae
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-20 12:23:27 +0430

    redux reducer

--- src/component/CartContainer.js
+++ src/component/CartContainer.js
@@ -33,7 +33,7 @@ const CartContainer = () => {
                         <span>${total}</span>
                     </h4>
                 </div>
-                <button onClick={() => dispatch} className="btn clear-btn">clear btn</button>
+                <button onClick={() => dispatch(clearCart)} className="btn clear-btn">clear btn</button>
             </footer>
         </section>
     )
