da1d8f80f807dc6f3406841f2f6ca46404c3bd60
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-20 11:00:38 +0430

    redux cartItem

--- src/component/CartItem.js
+++ src/component/CartItem.js
@@ -4,6 +4,8 @@ const CartItem = ({ id, img, title, price, amount }) => {
     return (
         <article className="cart-item">
             <img src={img} alt={title} />
+            <div>
+            </div>
         </article>
     )
 }
