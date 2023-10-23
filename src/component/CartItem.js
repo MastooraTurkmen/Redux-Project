8b0b1a22409f09be6f1f1bcefb5eabeb2e881d85
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-23 07:01:48 +0430

    redux reducer increase

--- src/component/CartItem.js
+++ src/component/CartItem.js
@@ -13,7 +13,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
                 <button onClick={() => dispatch(removeItem(id))} className="remove-btn">remove</button>
             </div>
             <div>
-                <button className="amount-btn">
+                <button onClick={() => dispatch(increaseItem(id))} className="amount-btn">
                     <ChevronUp />
                 </button>
                 <p className="amount">{amount}</p>
