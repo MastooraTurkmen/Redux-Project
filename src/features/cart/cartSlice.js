5d9bdca80119aaed8f66085e4f8c0573fdd7bdf5
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-23 07:08:59 +0430

    redux reducer decrease

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -20,11 +20,11 @@ const cartSlice = createSlice({
             state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
         },
         increaseItem: (state, { payload }) => {
-            const cartItem = state.cartItems.find((item) => item.id === payload)
+            const cartItem = state.cartItems.find((item) => item.id === payload.id)
             cartItem.amount = cartItem.amount + 1
         },
         decreaseItem: (state, { payload }) => {
-            const cartItem = state.cartItems.find((item) => item.id === payload)
+            const cartItem = state.cartItems.find((item) => item.id === payload.id)
             cartItem.amount = cartItem.amount - 1
         }
     }
