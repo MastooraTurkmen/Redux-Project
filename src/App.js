ea2431e59f0a71c928e893fc75be34cb69042c06
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-23 07:26:56 +0430

    redux reducer total

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./component/Navbar";
 import CartContainer from "./component/CartContainer";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { calculateTotal } from "./features/cart/cartSlice";
 
 function App() {
   return (
