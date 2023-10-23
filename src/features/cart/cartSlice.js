963490c71e58f8ac0745ce2bff6c7f8a6427e446
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-23 07:21:38 +0430

    redux reducer total

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -26,6 +26,9 @@ const cartSlice = createSlice({
         decreaseItem: (state, { payload }) => {
             const cartItem = state.cartItems.find((item) => item.id === payload.id)
             cartItem.amount = cartItem.amount - 1
+        },
+        calculateTotal: (state, { payload }) => {
+            
         }
     }
 })
