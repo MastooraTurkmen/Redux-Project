7746611accfcca6a3d3ff4b0965b81bcffec7701
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-24 07:38:58 +0430

    redux reducer modal

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -4,7 +4,7 @@ const Modal = () => {
     return (
         <aside className="modal-container">
             <div className="modal">
-                
+                <h4>remove all items from your shopping cart?</h4>
             </div>
         </aside>
     )
