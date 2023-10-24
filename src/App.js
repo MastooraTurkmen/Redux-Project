68dc822e43a9f6b438b8e53a678c1795326aa28f
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-24 07:41:04 +0430

    redux reducer modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CartContainer from "./component/CartContainer";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { calculateTotal } from "./features/cart/cartSlice";
+import Modal from "./component/Modal";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart)
