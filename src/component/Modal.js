472d70b26d681f1bad2809a84590253a5cde7e4e
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-24 08:25:03 +0430

    redux reducer modal

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -4,12 +4,13 @@ import { clearCart } from '../features/cart/cartSlice'
 import { useDispatch } from 'react-redux'
 
 const Modal = () => {
+    const dispatch = useDispatch()
     return (
         <aside className="modal-container">
             <div className="modal">
                 <h4>remove all items from your shopping cart?</h4>
                 <div className="btn-container">
-                    <button className="btn confirm-btn">confirm</button>
+                    <button onClick={()=>} className="btn confirm-btn">confirm</button>
                     <button className="btn clear-btn">cancel</button>
                 </div>
             </div>
