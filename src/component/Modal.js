10cfe84919bc349493a5a9e6b8459076cd983a3c
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-24 07:40:30 +0430

    redux reducer modal

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -7,6 +7,7 @@ const Modal = () => {
                 <h4>remove all items from your shopping cart?</h4>
                 <div className="btn-container">
                     <button className="btn confirm-btn">confirm</button>
+                    <button className="btn clear-btn">cancel</button>
                 </div>
             </div>
         </aside>
