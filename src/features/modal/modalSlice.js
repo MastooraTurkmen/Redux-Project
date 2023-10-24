931d052f483221926fc065e9aabf2db2a0b51b85
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-24 07:49:53 +0430

    redux reducer modal

--- src/features/modal/modalSlice.js
+++ src/features/modal/modalSlice.js
@@ -17,6 +17,6 @@ const modalSlice = createSlice({
     }
 })
 
-export const { openModal } = modalSlice.actions;
+export const { openModal, closeModal } = modalSlice.actions;
 
 export default modalSlice.reducer;
