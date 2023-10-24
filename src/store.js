d0b80905250ef1b02dd0fe0d8f9990b2d7915d2e
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-24 08:16:59 +0430

    redux reducer modal

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './features/cart/cartSlice';
-import modalReducer from './features/cart/cartSlice';
+import modalReducer from './features/cart/modalSlice';
 
 export const store = configureStore({
     reducer: {
