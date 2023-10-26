1eff54ae5d2c67053ef2328005b2644f51fca5c9
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-26 10:36:21 +0430

    thunkAPI

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -15,7 +15,7 @@ export const getCartItems = createAsyncThunk('cart/getCartItems', async (name, t
         const respons = await axios(url)
         return respons.data;
     } catch (error) {
-        console.log(error);
+        return thunkAPI.rejectWithValue('something went wrong')
     }
 })
 
