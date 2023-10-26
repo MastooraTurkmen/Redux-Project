e7240beafaf8c464466c7b83d9e762ca4bec4768
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-26 09:36:49 +0430

    redux reducer fetch async thunk

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
 
 const initialState = {
